c8fd5682462b8e07e3e3215d4bdfe7d12f6685f3
jammorata <jam_morata@yahoo.es>
2017-02-13 19:46:41 +0100

    add folder api javascript,simple map y templates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,4 @@
 (function () {
    'use strict';
    // this function is strict...
-   var mi_imagen = document.getElementById('mi_imagen');
-   mi_imagen.style = 'color: red';
-   console.log('mi_imagen = ', mi_imagen.getAttribute('style'));
-}());
+   }());
