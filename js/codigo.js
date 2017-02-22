2e3217475a0fd7282bd14a3de664e1512c868848
jammorata <jam_morata@yahoo.es>
2017-02-22 12:10:44 +0100

    update about.json and upload exercise widget,pob usa

--- js/codigo.js
+++ js/codigo.js
@@ -4,5 +4,5 @@
    }());
 
    alert("Hola mundo");
-   alert("Soy el primer script")
+   alert("Soy el primer script");
    /*primeros comentarios para el ejercicio uno del manual crear mis scripts*/
